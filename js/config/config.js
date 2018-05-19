fc6a402950fea083745fa0cee6d58f0a168a97c3
chatrapati <sivajitc@gmail.com>
2018-05-19 12:26:11 +0530

    nani
    nani

--- js/config/config.js
+++ js/config/config.js
@@ -28,5 +28,5 @@ shopMyToolsApp.constant('LOGIN_URL', ' http://157.119.108.135:8005');
 
 shopMyToolsApp.constant('PRODUCT_DETAIL_SERVICE', 'http://157.119.108.139:8005');
 shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://157.119.108.137:80');
- shopMyToolsApp.constant('DOMAIN_URL','http://localhost/smtsitewithangular/SmtSite/');
+ shopMyToolsApp.constant('DOMAIN_URL','http://localhost/SmtSiteAngular/SmtSite/');
 
