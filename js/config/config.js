04375007e873e42c499d94f2ad46facf132b6a70
Your Name <sivajitc@gmail.com>
2018-06-13 14:38:27 +0530

    nani
    nani

--- js/config/config.js
+++ js/config/config.js
@@ -7,7 +7,7 @@ shopMyToolsApp.constant('LOGIN_URL', ' http://157.119.108.135:8005');
 
 shopMyToolsApp.constant('PRODUCT_DETAIL_SERVICE', 'http://157.119.108.139:8005');
 
-shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://157.119.108.137:80');
+// shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://157.119.108.137:80');
 
 
 
@@ -20,11 +20,11 @@ shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://157.119.108.137:80')
 
 //  shopMyToolsApp.constant('PRODUCT_DETAIL_SERVICE', 'http://192.168.20.107:8090');
 
-//  shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://192.168.20.64:8002');
+ shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://192.168.20.64:8002');
 
-shopMyToolsApp.constant('DOMAIN_URL','http://localhost/smtsitenew/SmtSite/');
+shopMyToolsApp.constant('DOMAIN_URL','http://localhost/smt/SmtSite/');
 
-shopMyToolsApp.constant('BASE_URL','/smtsitenew/SmtSite/');
+shopMyToolsApp.constant('BASE_URL','/smt/SmtSite/');
 
 
 
