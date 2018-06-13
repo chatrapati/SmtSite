c6d4a51587ad28b69f015b465716de8f39755be4
chatrapati <sivajitc@gmail.com>
2018-06-13 12:54:12 -0700

    sruthi
    sruthi

--- js/config/config.js
+++ js/config/config.js
@@ -3,7 +3,7 @@ shopMyToolsApp.constant('SERVER_URL', 'http://157.119.108.142:8002');
 
 shopMyToolsApp.constant('SERVER_URL1', 'http://157.119.108.142:8002');
 
-shopMyToolsApp.constant('LOGIN_URL', ' http://157.119.108.135:8005');
+// shopMyToolsApp.constant('LOGIN_URL', ' http://157.119.108.135:8005');
 
 shopMyToolsApp.constant('PRODUCT_DETAIL_SERVICE', 'http://157.119.108.139:8005');
 
@@ -16,7 +16,7 @@ shopMyToolsApp.constant('PRODUCT_CATEGORY_SERVICE', 'http://157.119.108.137:80')
 
 //   shopMyToolsApp.constant('SERVER_URL1', 'http://192.168.20.66:8000');
 
-//  shopMyToolsApp.constant('LOGIN_URL', 'http://192.168.20.67:8080');
+ shopMyToolsApp.constant('LOGIN_URL', 'http://192.168.20.67:8080');
 
 //  shopMyToolsApp.constant('PRODUCT_DETAIL_SERVICE', 'http://192.168.20.107:8090');
 
